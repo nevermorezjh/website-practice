aae8466dfb366b79ddf64b1b14fc787aa3ab446f
nevermorezjh <nevermorezjh@hotmail.com>
2019-03-07 17:21:31 +0800

    improve stockpools

--- static/js/quant.js
+++ static/js/quant.js
@@ -1,7 +1,6 @@
-
-
 function initialize() {
-    var unselected_stockpool = document.getElementById("unselectedstockpool");
-    unselected_stockpool.options.add(new Option("text", "value"));
+    var unselected_stockpool = document.getElementById("unselected-stockpool");
+    unselected_stockpool.options.add(new Option("ni", "xxx"));
+    unselected_stockpool.options.add(new Option("wo", "yyy"));
     console.log(unselected_stockpool);
 }
